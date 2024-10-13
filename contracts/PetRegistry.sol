f2e23a2cb273f3e42f032f2ea45225ea2fd17642
rbragadev <raffael.info@gmail.com>
2024-10-13 10:46:31 -0300

    feat/payable functions

--- contracts/PetRegistry.sol
+++ contracts/PetRegistry.sol
@@ -19,12 +19,21 @@ contract PetRegistry {
     mapping(uint256 => Pet) public pets;
 
     uint256 public counter;
+    //in prod we use wei
+    uint256 public registrationFee = 0.01 ether;
+    address public owner;
 
     constructor (){
         counter = 0;
+        owner = msg.sender;
     }
 
     event PetRegistered(uint256 indexed petId, address indexed owner, string name);
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Only owner of contract can execute this function.");
+        _;
+    }
 
     function _generatePetId() internal returns (uint256){
         uint256 petId = counter + 1;
@@ -52,7 +61,16 @@ contract PetRegistry {
         string memory _color,
         uint256 _birthDate,
         uint256 _weight
-    ) public {
+    ) public payable  {
+        require(msg.value >= registrationFee, "not enough funds");
         _createPet(_name, _species, _breed, _color, _birthDate, _weight);
+    }
+
+    function withdraw() public onlyOwner {
+        payable(owner).transfer(address(this).balance);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }
