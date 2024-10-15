de1525ed7d87380683364f9d0456aa1bf1864128
rbragadev <raffael.info@gmail.com>
2024-10-15 08:35:25 -0300

    feat/implement chainlink oracle

--- contracts/PetRegistry.sol
+++ contracts/PetRegistry.sol
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract PetRegistry {
+
+    AggregatorV3Interface internal priceFeed;
+
+    address public priceFeedAddress;
     
     enum Species {Dog, Cat, Bird, Other}
 
@@ -20,12 +26,15 @@ contract PetRegistry {
 
     uint256 public counter;
     //in prod we use wei
-    uint256 public registrationFee = 0.01 ether;
+    uint256 public usdRegistrationFee;
     address public owner;
 
     constructor (){
         counter = 0;
         owner = msg.sender;
+        usdRegistrationFee = 10;
+        priceFeedAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     event PetRegistered(uint256 indexed petId, address indexed owner, string name);
@@ -33,6 +42,28 @@ contract PetRegistry {
     modifier onlyOwner(){
         require(msg.sender == owner, "Only owner of contract can execute this function.");
         _;
+    }
+
+    function getEthPriceInUsd() public view returns (uint256){
+        (
+            /* uint80 roundID */,
+            int256 price,
+            /* uint startedAt */,
+            /* uint timeStamp */,
+            /* uint80 answeredInRound */
+        ) = priceFeed.latestRoundData();
+        
+        return uint256(price / 10 ** 8);
+    }
+
+    function getEthRegistrationFeeInWei() public view returns (uint256) {
+        uint256 ethPrice = getEthPriceInUsd();
+        uint256 feeInWei = (usdRegistrationFee * 10 ** 18) / ethPrice;
+        return feeInWei;
+    }
+
+    function updateRegistrationFee(uint256 _newFee) public onlyOwner {
+        usdRegistrationFee = _newFee;
     }
 
     function _generatePetId() internal returns (uint256){
@@ -62,7 +93,8 @@ contract PetRegistry {
         uint256 _birthDate,
         uint256 _weight
     ) public payable  {
-        require(msg.value >= registrationFee, "not enough funds");
+        uint256 requiredFee = getEthRegistrationFeeInWei(); 
+        require(msg.value >= requiredFee, "not enough funds");
         _createPet(_name, _species, _breed, _color, _birthDate, _weight);
     }
 
